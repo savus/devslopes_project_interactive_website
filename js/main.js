dd62befa8964c1c9e26221620049f79cd7df58bc
Savus <justin.fuzzygreenstuff@gmail.com>
2023-03-04 01:23:05 -0700

    Add Contact and About page modals and hook up animations through javascript

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const modalOpen = '[data-open]';
+const modalClose = '[data-close]';
+const isVisible = 'is-visible';
+
+const openModal = document.querySelectorAll(modalOpen);
+const closeModal = document.querySelectorAll(modalClose);
+
+// Full site modal 'open buttons'
+for (const elm of openModal) {
+   elm.addEventListener('click', function() {
+      const modalId = this.dataset.open;
+      document.getElementById(modalId).classList.add(isVisible);
+   });
+}
+
+for (const elm of closeModal) {
+   elm.addEventListener('click', function() {
+      this.parentElement.parentElement.classList.remove(isVisible);
+   });
+}
