1f8222332cb3056ac7612794422463a338a6c84f
Savus <justin.fuzzygreenstuff@gmail.com>
2023-03-30 23:34:15 -0600

    Add javascript functions to toggle the theme of index.html

--- js/main.js
+++ js/main.js
@@ -19,6 +19,40 @@ const toggleTheme = document.querySelector(themeTab);
 const switcher = document.querySelectorAll(switcherBtn);
 const currentTheme = localStorage.getItem(theme);
 
+toggleTheme.addEventListener('click', function(){
+   const tab = this.parentElement.parentElement;
+   if (!tab.className.includes(open)) {
+      tab.classList.add(open);
+   } else {
+      tab.classList.remove(open);
+   }
+});
+
+const setActive = (elm, selector) => {
+   if (document.querySelector(`${selector}.${active}`) !== null) {
+      document.querySelector(`${selector}.${active}`).classList.remove(active);
+   } else {
+      elm.classList.add(active);
+   }
+};
+
+const setTheme = (val) => {
+   if (val === dark) {
+      root.setAttribute(dataTheme, dark);
+      localStorage.setItem(theme, dark);
+   } else {
+      root.setAttribute(dataTheme, light);
+      localStorage.setItem(theme, light);
+   }
+}
+
+for (const elm of switcher) {
+   elm.addEventListener('click', function(){
+      const toggle = this.dataset.toggle;
+      setActive(elm, switcherBtn);
+      setTheme(toggle);
+   });
+}
 
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
@@ -35,4 +69,4 @@ for (const elm of closeModal) {
    elm.addEventListener('click', function() {
       this.parentElement.parentElement.classList.remove(isVisible);
    });
-}
+}  
